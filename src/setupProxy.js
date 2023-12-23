5a8adc496184267276017c498512d3b39667ef33
Stefan Haun <mail@tuxathome.de>
2023-12-23 15:31:10 +0100

    Setup proxy target from environment if available

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const default_backend_url = "http://localhost:8080";
+
+
+
 module.exports = function (app) {
     app.use(
         '/backend',
         createProxyMiddleware({
-            target: 'http://localhost:8080',
+            target: window.ENV && window.ENV.REACT_APP_WEB_BACKEND
+                ? window.ENV.REACT_APP_WEB_BACKEND + "/"
+                : default_backend_url + "/",
             changeOrigin: true,
             pathRewrite: {
                 '^/backend': ''
